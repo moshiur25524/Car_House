20368d9f8c28976509861e80a4b61378c8c3ae82
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-10-26 23:44:39 +0600

    All Routes are created in the header section

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Contact from './Pages/Contact/Contact';
 import NotFound from './Pages/NotFound/NotFound';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Reviews from './Pages/Reviews/Reviews';
+import Products from './Pages/Products/Products';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
         <Route path='/home' element={<Home></Home>} />
         <Route path='/about' element={<About></About>} />
         <Route path='/contact' element={<Contact/>} />
+        <Route path='/reviews' element={<Reviews/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        <Route path='/blogs' element={<Blogs/>} />
+        <Route path='/products' element={<Products/>} />
         <Route path='*' element={<NotFound/>} />
       </Routes>
     </div>
