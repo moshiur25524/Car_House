75c0d0b89cd7f74529f20b998f8fa5a87afca3f1
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-10-26 23:31:31 +0600

    Car House Website's work is started

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1>This is Footer part</h1>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from './Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            {/* <h1>This is Home page</h1> */}
+            <Banner/>
+        </div>
+    );
+};
+
+export default Home;
