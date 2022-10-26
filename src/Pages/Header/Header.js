d6588a87d5de11996c5b829d06dfbd0f53e71076
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-10-27 01:00:03 +0600

    Worked with Active route but didn't solved

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -5,15 +5,15 @@ import carIcon from '../../Resources/Images/car-icon-design-free-vector.webp'
 const Header = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-light">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to='/'>
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <Link class="navbar-brand" to='/'>
                     <img src={carIcon} alt="Bootstrap" width="30" height="24"/>
                     </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
@@ -23,20 +23,21 @@ const Header = () => {
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to='/contact'>Contact</Link>
-                            </li> 
+                            </li>
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to='/reviews'>Reviews</Link>
-                            </li> 
+                            </li>
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to='/dashboard'>Dashboard</Link>
-                            </li> 
+                            </li>
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to='/blogs'>Blogs</Link>
-                            </li> 
+                            </li>
                             <li className="nav-item">
                                 <Link className="nav-link active" aria-current="page" to='/products'>Products</Link>
-                            </li> 
+                            </li>
                         </ul>
+
                     </div>
                 </div>
             </nav>
