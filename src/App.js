3802e33f09f013a1c848444800d349e759ccd237
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-10-30 00:56:22 +0600

    created a button to go to the top of the Page

--- src/App.js
+++ src/App.js
@@ -10,8 +10,13 @@ import Reviews from './Pages/Reviews/Reviews';
 import Products from './Pages/Products/Products';
 import Blogs from './Pages/Blogs/Blogs';
 import Footer from './Pages/Footer/Footer';
+import { useEffect } from 'react';
+import { faBorderNone } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+  }, [])
   return (
     <div>
       <Header></Header>
@@ -19,14 +24,33 @@ function App() {
         <Route path='/' element={<Home></Home>} />
         <Route path='/home' element={<Home></Home>} />
         <Route path='/about' element={<About></About>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/reviews' element={<Reviews/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-        <Route path='/blogs' element={<Blogs/>} />
-        <Route path='/products' element={<Products/>} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/reviews' element={<Reviews />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
-      <Footer/>
+      <Footer />
+      <button
+        onClick={() => {
+          window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+        }}
+        style={{
+          border: 'none',
+          borderRadius:["10%"],
+          position: 'fixed',
+          padding: '1rem 2rem',
+          fontSize: '20px',
+          bottom: '40px',
+          right: '40px',
+          backgroundColor: '#0C9',
+          color: '#fff',
+          textAlign: 'center',
+        }}
+      >
+       ↑ Top
+      </button>
     </div>
   );
 }
