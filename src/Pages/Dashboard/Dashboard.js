d919a211d8b91d2442c6b2232cabe1b1624ceac0
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-10-29 14:12:55 +0600

    animation is added in dashboard using react-spring

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MyAreaChart from '../Chart/MyAreaChart';
 import MyLineChart from '../Chart/MyLineChart';
 import MyPieChart from '../Chart/MyPieChart';
 import MySpecialChart from '../Chart/MySpecialChart';
+import { useSpring, animated } from 'react-spring'
 
 const Dashboard = () => {
+    const [flip, set] = useState(false)
+    const props = useSpring({
+      to: { opacity: 1 },
+      from: { opacity: 0 },
+      reset: true,
+      reverse: flip,
+      delay: 200,
+      onRest: () => set(!flip),
+    })
     return (
         <div>
-            <h1 className='text-center'>Full Production Records</h1>
+            <animated.h1 style={props} className='text-center'>Full Production Records</animated.h1>
             <div className='d-flex align-items-center justify-content-center flex-column'>
             <MyLineChart></MyLineChart>
             <h2>Area chart of prouducts</h2>
